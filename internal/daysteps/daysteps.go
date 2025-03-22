f5c81de0848ccea6157f9569aa14d07bb677b49d
Vladimir Kalinkin <deyork92@gmail.com>
2025-03-22 19:01:34 +0300

    first commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,6 +1,11 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"github.com/Yandex-Practicum/go1fl-4-sprint-final/internal/spentcalories"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -9,7 +14,26 @@ var (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// ваш код ниже
+	var (
+		steps    int
+		duration time.Duration
+		err      error
+	)
+
+	arr := strings.Split(data, ",")
+	if len(arr) == 2 {
+		steps, err = strconv.Atoi(arr[0])
+		if err != nil || steps <= 0 {
+			return 0, 0, err
+		}
+		duration, err = time.ParseDuration(arr[1])
+		if err != nil {
+			return 0, 0, err
+		}
+	} else {
+		return 0, 0, errors.New("Invalid input")
+	}
+	return steps, duration, nil
 }
 
 // DayActionInfo обрабатывает входящий пакет, который передаётся в
@@ -19,5 +43,13 @@ func parsePackage(data string) (int, time.Duration, error) {
 // Если пакет валидный, он добавляется в слайс storage, который возвращает
 // функция. Если пакет невалидный, storage возвращается без изменений.
 func DayActionInfo(data string, weight, height float64) string {
-	// ваш код ниже
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	distance := (StepLength * float64(steps)) / 1000
+	calories := spentcalories.WalkingSpentCalories(steps, weight, height, duration)
+	return fmt.Sprint("Количество шагов: %d.\nДистанция составила %f км.\nВы сожгли %f каллорий.",
+		steps, distance, calories)
 }
